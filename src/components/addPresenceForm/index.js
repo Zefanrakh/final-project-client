78372cd44ca2246e06930dfb1969978075e58afe
dudebh <dudebahrulhayat@gmail.com>
2021-06-27 19:47:09 +0700

    get qr code

--- src/components/addPresenceForm/index.js
+++ src/components/addPresenceForm/index.js
@@ -31,6 +31,8 @@ const AddMemberForm = ({ openPopUpHandler }) => {
   const [appointmentData, setAppointmentData] = useState({});
   const [appointmentChoosed, setAppointmentChoosed] = useState([]);
   const [inputAppointmentValue, setInputAppointmentValue] = useState([]);
+  const [showQrcode, setShowQrcode] = useState(false)
+  const [linkAkses, setLinkAkses] = useState("")
   const [inputForm, setInputForm] = useState({
     AppointmentId: "",
     dropperName: "",
@@ -60,6 +62,8 @@ const AddMemberForm = ({ openPopUpHandler }) => {
     dispatch(addPresence(inputForm))
     .then(({data}) => {
       console.log(data);
+      setLinkAkses("http://localhost:3000/monitoring")
+      setShowQrcode(true)
     })
     .catch(err=>{
       console.log(err);
@@ -68,50 +72,56 @@ const AddMemberForm = ({ openPopUpHandler }) => {
   return (
     <div className="overlay">
       <div className="add-presence-container ">
-        <div className="title">Add Presence</div>
-        <i className="fas fa-times icon-close" onClick={openPopUpHandler}></i>
-        <form onSubmit={createPresence}>
-        <QRCode value="hey"/>
-          <label>Find Appointment</label>
-          <input
-            type="text"
-            onChange={onChangeHandler}
-            value={
-              isEmpty(appointmentChoosed)
-                ? inputAppointmentValue
-                : appointmentChoosed.childName
-            }
-          />
-          {!isEmpty(appointmentData) && isEmpty(appointmentChoosed) && (
-            <div className="find-customer">
-              {appointmentData.map((appointment, idx) => {
-                return (
-                  <div className="customer-name__container" key={idx}>
-                    <div className="customer-name">{appointment.childName} - {appointment.Customer.name}</div>
-                    <div
-                      className="text-choose"
-                      onClick={() => {setAppointmentChoosed(appointment)
-                        setInputForm({...inputForm, AppointmentId: appointment.id})
-                      }}
-                    >
-                      choose
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-          )}
-          <label>Dropper</label>
-          <input type="text" placeholder="Masukan Dropper" onChange={(e) => setInputForm({...inputForm, dropperName: e.target.value})}/>
-          <label>Pickuper</label>
-          <input type="text" placeholder="Masukan Pickuper" onChange={(e) => setInputForm({...inputForm, pickupperName: e.target.value})}/>
-          <label>Date</label>
-          <input type="date" onChange={(e) => setInputForm({...inputForm, presenceDate: e.target.value})}/>
-          <label>Time</label>
-          <input type="time" onChange={(e) => setInputForm({...inputForm, pickupTime: e.target.value})}/>
+      {
+        !showQrcode ? (
+          <>
+            <div className="title">Add Presence</div>
+            <i className="fas fa-times icon-close" onClick={openPopUpHandler}></i>
+            <form onSubmit={createPresence}>
+            {/* <QRCode value="hey"/> */}
+              <label>Find Appointment</label>
+              <input
+                type="text"
+                onChange={onChangeHandler}
+                value={
+                  isEmpty(appointmentChoosed)
+                    ? inputAppointmentValue
+                    : appointmentChoosed.childName
+                }
+              />
+              {!isEmpty(appointmentData) && isEmpty(appointmentChoosed) && (
+                <div className="find-customer">
+                  {appointmentData.map((appointment, idx) => {
+                    return (
+                      <div className="customer-name__container" key={idx}>
+                        <div className="customer-name">{appointment.childName} - {appointment.Customer.name}</div>
+                        <div
+                          className="text-choose"
+                          onClick={() => {setAppointmentChoosed(appointment)
+                            setInputForm({...inputForm, AppointmentId: appointment.id})
+                          }}
+                        >
+                          choose
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              )}
+              <label>Dropper</label>
+              <input type="text" placeholder="Masukan Dropper" onChange={(e) => setInputForm({...inputForm, dropperName: e.target.value})}/>
+              <label>Pickuper</label>
+              <input type="text" placeholder="Masukan Pickuper" onChange={(e) => setInputForm({...inputForm, pickupperName: e.target.value})}/>
+              <label>Date</label>
+              <input type="date" onChange={(e) => setInputForm({...inputForm, presenceDate: e.target.value})}/>
+              <label>Time</label>
+              <input type="time" onChange={(e) => setInputForm({...inputForm, pickupTime: e.target.value})}/>
 
-          <button>Submit</button>
-        </form>
+              <button>Submit</button>
+            </form>
+          </>
+        ): <QRCode value={linkAkses}/>
+      }
       </div>
     </div>
   );
