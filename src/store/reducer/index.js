1889e9f04590c24f9dc04edeb372263cf70c2e83
azri fathul <azrifathulabar@gmail.com>
2021-06-28 09:48:27 +0700

    merge from

--- src/store/reducer/index.js
+++ src/store/reducer/index.js
@@ -1,11 +1,15 @@
 import { combineReducers } from "redux";
 import dummyReducer from "./dummyReducer";
 import userReducer from "./userReducer";
-import fetchDummyReducer from "./fetchDummyReducer";
-import fetchAppointmentReducer from "./fetchAppointmentReducer"
-import fetchCustomerReducer from "./fetchCustomerReducer"
-import fetchPresenceReducer from "./fetchPresenceReducer"
+// import fetchDummyReducer from "./fetchDummyReducer";
+import fetchAppointmentReducer from "./fetchAppointmentReducer";
+import fetchCustomerReducer from "./fetchCustomerReducer";
+import fetchPresenceReducer from "./fetchPresenceReducer";
 
 export default combineReducers({
-  fetchDummyReducer, fetchAppointmentReducer, fetchCustomerReducer, fetchPresenceReducer, serReducer
+  // fetchDummyReducer,
+  fetchAppointmentReducer,
+  fetchCustomerReducer,
+  fetchPresenceReducer,
+  serReducer,
 });
