8e9ba8eb7ac8e5be3a83a96f4a0b034438ea9f89
azri fathul <azrifathulabar@gmail.com>
2021-06-28 16:47:22 +0700

    comit

--- src/config/index.js
+++ src/config/index.js
@@ -1,17 +1,22 @@
 export const config = {
   iceServers: [
-    {
-      urls: "stun:stun.stunprotocol.org",
-    },
+    // {
+    //   urls: "stun:stun.stunprotocol.org",
+    // },
     // {
     //   urls: "turn:numb.viagenie.ca",
     //   credential: "muazkh",
     //   username: "webrtc@live.com",
     // },
     {
+      url: "stun:global.stun.twilio.com:3478?transport=udp",
+      urls: "stun:global.stun.twilio.com:3478?transport=udp",
+    },
+    {
       username:
         "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
       credential: "tE2DajzSJwnsSbc123",
+      url: "turn:global.turn.twilio.com:3478?transport=udp",
       urls: "turn:global.turn.twilio.com:3478?transport=udp",
     },
   ],
