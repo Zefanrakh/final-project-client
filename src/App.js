19947b66b86b7a33b93ff8f9fdc6998dd244157e
azri fathul <azrifathulabar@gmail.com>
2021-06-29 09:03:15 +0700

    add page not found

--- src/App.js
+++ src/App.js
@@ -12,20 +12,20 @@ function App() {
 
   useEffect(() => {
     const access_token = localStorage.access_token;
-    // if (access_token) {
-    //   axios("http://localhost:3001" + "/user/getdata", {
-    //     method: "POST",
-    //     headers: {
-    //       access_token,
-    //     },
-    //   })
-    //     .then((res) => {
-    //       dispatch(setUserAction(res.data.user));
-    //     })
-    //     .catch((err) => {
-    //       console.log(err);
-    //     });
-    // }
+    if (access_token) {
+      axios("http://localhost:3001" + "/user/getdata", {
+        method: "POST",
+        headers: {
+          access_token,
+        },
+      })
+        .then((res) => {
+          dispatch(setUserAction(res.data.user));
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
   }, []);
   console.log(user);
   return (
